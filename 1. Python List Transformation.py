8f981cbf67cc119650dd26f61a6fde9d618d2fe5
jairt7 <jaredraywilson@gmail.com>
2024-09-17 10:58:39 -0600

    Add files

---
+++ 1. Python List Transformation.py
@@ -0,0 +1,9 @@
+# 1. Python List Transformation
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+grades.reverse()
+print("Grades, sorted in descending order:")
+print(grades)
+grade_average = sum(grades) / len(grades)
+print("Grade average:", grade_average)
---
+++ 2. Advanced List Methods and Identity Operators.py
@@ -0,0 +1,9 @@
+# 2. Advanced List Methods and Identity Operators
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice submitted her assignment and attended class.")
+else:
+    print("Alice did not submit her assignment and attend class.")
